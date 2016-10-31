d50c89ee2a4162a257a4289488ee39f78c72a9ec
Jack Gommeren <Gommeren@msn.com>
2016-10-31 23:39:42 +0100

    Added dummy files to finish project

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
-$(document).foundation();
+$(document).foundation({
+        orbit: {
+            timer_speed: 2000,
+            pause_on_hover: false,
+            animation_speed: 1000,
+            slide_number: false,
+            navigation_arrows: true,
+            variable_height: false,
+        }
+});
